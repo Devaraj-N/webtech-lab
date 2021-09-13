f2302c1df0703cb5ef8465c178951d0b2c20bb6c
Devaraj-N <68743390+Devaraj-N@users.noreply.github.com>
2021-09-13 19:59:21 +0530

    Create contractdet.php

---
+++ lab6/contractdet.php
@@ -0,0 +1,17 @@
+<?php
+// Load XML file
+$xml = new DOMDocument;
+$xml->load('contractdet.xml');
+
+// Load XSL file
+$xsl = new DOMDocument;
+$xsl->load('contractdet.xsl');
+
+// Configure the transformer
+$proc = new XSLTProcessor;
+
+// Attach the xsl rules
+$proc->importStyleSheet($xsl);
+
+echo $proc->transformToXML($xml);
+?>
